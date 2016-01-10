35b59c5ed83b3c965a6970eb2ed3fe4334f2c567
Byung Ju Hwang <pjhwang@gmail.com>
2016-01-10 14:15:50 +0900

    update git build script
    - add build-dep packages
    - version up to v2.7.0

--- install-git-from-src.sh
+++ install-git-from-src.sh
@@ -1,25 +1,47 @@
 #!/bin/bash
 
-GIT_VERSION=2.6.1
-GIT_FILE=v${GIT_VERSION}.zip
-GIT_URL=https://github.com/git/git/archive/${GIT_FILE}
+GIT_VERSION="2.7.0"
+GIT_FILE="v${GIT_VERSION}.zip"
+GIT_URL="https://github.com/git/git/archive/${GIT_FILE}"
 
-do_clean() {
-    rm -rf git-${GIT_VERSION} ; 
+
+function do_checkenv {
+    sudo apt-get install -y autoconf automake zlib1g-dev asciidoc
+}
+
+function do_clean {
+    rm -rf git-${GIT_VERSION} ;
     rm -f ${GIT_FILE} ;
 }
 
-do_fetch() {
-    wget $GIT_URL ;
-    unzip $GIT_FILE ;
+function do_fetch {
+
+    if [[ ! -f ${GIT_FILE} ]]; then
+	wget $GIT_URL
+    fi
+
+    if command -v unzip 2> /dev/null; then
+	unzip $GIT_FILE
+    else
+	sudo apt-get install -y unzip
+	unzip $GIT_FILE
+    fi
 }
 
-do_build() {
-    cd git-${GIT_VERSION} ;
-    make configure ;
-    ./configure --prefix=/usr ;
-    make all doc ;
-    sudo make install install-doc ;
+function do_build {
+    cd git-${GIT_VERSION}
+    make configure
+
+    if [[ ! -f ./configure ]]; then
+	exit "the configure is not found."
+    fi
+
+    ./configure --prefix=/usr
+    make all doc
+    sudo make install install-doc
 }
 
+set -e
+
+do_checkenv
 do_clean && do_fetch && do_build
