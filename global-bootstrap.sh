0cf91bb8aecb8a66532cc8fba556dd7087bf0151
ByungJu Hwang <pjhwang@gmail.com>
2019-06-26 08:24:17 +0000

    global: bump version

--- global-bootstrap.sh
+++ global-bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PROGRAM_NAME="global"
-VERSION="6.6.2"
+VERSION="6.6.3"
 SRC_FILE="${PROGRAM_NAME}-${VERSION}.tar.gz"
 SRC_URL="ftp://ftp.gnu.org/pub/gnu/global/${SRC_FILE}"
 CONFIGURE_OPTIONS=" \
